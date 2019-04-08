38471ef5c4034fb063920932fe99f5a6554e86e2
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-08 00:45:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,13 +7,13 @@ def reverse_each_word(string)
   return_array.join(" ")
 end
 
-
-
-
-def change_nums(nums)
-  nums.collect do |x| 
-    x + 1
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  return_array = []
+  new_array.collect do |word|
+    return_array.push word.reverse
   end
+  return_array.join(" ")
 end
 
 
